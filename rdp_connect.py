27ddb4618ed72a0dce80f16b082c2cac3ad4531f
luckyaddress <deathkao@gmail.com>
2018-02-09 16:13:12 +0800

    commit

--- rdp_connect.py
+++ rdp_connect.py
@@ -1,17 +1,12 @@
-## 將要執行遠端桌面的主機，以及相關參數列在下方二維陣列中
-## 一維陣列參數為 ["子網域", "ip起點", "ip終點" ,"帳號" ,"密碼"] 
 
-import os 
+import os, subnet_ip_data # 將陣列資料，存在subnet_ip_data中
 
-host_array = [
-    ["subnet.", 222 , 222, "admin", "1234"],
-    ["subnet2.", 111 , 111, "admin", "1234"]
-]
+content = subnet_ip_data.host_array # 將陣列資料，指定給一個變數，便於使用
 
-command = ""
+command = ""  # 設定一個全域變數，儲存後續執行的系統指令
 
-for i in range (0, len(host_array),1): # 取得第一個參數 subnet
-    for ip in range(host_array[i][1], host_array[i][2]+1,1): # 組出 指令
-        command = "xfreerdp " + "-u " + host_array[i][3] + " -p " + host_array[i][4] + " --ignore-certificate " + " " +  host_array[i][0]  + str(ip) + " &" 
+for i in range (0, len(content),1): # 取得第一個參數 subnet
+    for ip in range(content[i][1], content[i][2]+1,1): # 組出 指令
+        command = "xfreerdp " + "-u " + content[i][3] + " -p " + content[i][4] + " --ignore-certificate " + " " +  content[i][0]  + str(ip) + " &" 
         print(command)
-        #os.system(command) #執行系統命令
+        os.system(command) #執行系統命令
