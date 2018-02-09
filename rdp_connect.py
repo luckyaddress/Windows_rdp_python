119436414e09344a45f3e43780f5abbd28177e77
luckyaddress <deathkao@gmail.com>
2018-02-09 16:13:45 +0800

    commit

--- rdp_connect.py
+++ rdp_connect.py
@@ -9,4 +9,4 @@ for i in range (0, len(content),1): # 取得第一個參數 subnet
     for ip in range(content[i][1], content[i][2]+1,1): # 組出 指令
         command = "xfreerdp " + "-u " + content[i][3] + " -p " + content[i][4] + " --ignore-certificate " + " " +  content[i][0]  + str(ip) + " &" 
         print(command)
-        os.system(command) #執行系統命令
+        #os.system(command) #執行系統命令
